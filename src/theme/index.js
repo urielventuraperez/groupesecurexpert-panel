e8d2874aecd5da985cfa83d87451cafb3c7e14a0
Uriel <venturaperez.10101991@gmail.com>
2021-02-02 07:40:12 -0600

    Feature: new theme colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,7 +10,7 @@ const theme = createMuiTheme({
       paper: colors.common.white
     },
     primary: {
-      main: colors.grey[900]
+      main: colors.blue[700]
     },
     secondary: {
       main: colors.grey[500]
