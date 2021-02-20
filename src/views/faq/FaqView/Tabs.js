708458e34e49f8e1a3cafee3590aec0e61143dd6
Uriel <venturaperez.10101991@gmail.com>
2021-02-20 05:47:52 -0600

    feature: panel expanded tabs

--- src/views/faq/FaqView/Tabs.js
+++ src/views/faq/FaqView/Tabs.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionActions from '@material-ui/core/AccordionActions';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -11,6 +12,7 @@ import Divider from '@material-ui/core/Divider';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
 import FaqDialog from './Form';
 
 const useStyles = makeStyles(theme => ({
@@ -30,6 +32,12 @@ const TabsFaq = props => {
 
   const [open, setOpen] = useState(false);
 
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -39,44 +47,50 @@ const TabsFaq = props => {
   };
 
   return (
-    <Card>
-      <CardHeader
-        subheader="Manage the frequent answer questions"
-        title="FAQ"
-      />
-      <Divider />
-      <CardContent>
-        <Typography color="textPrimary" gutterBottom variant="h6">
-          List
-        </Typography>
-
-        {props.faqs.map(faq => (
-          <Accordion key={faq.id}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className={classes.heading}>{faq.name}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {faq.status}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))}
-        <Fab
-          onClick={handleClickOpen}
-          className={classes.fab}
-          color="primary"
-          aria-label="add"
-        >
-          <AddIcon />
-        </Fab>
-      </CardContent>
+    <div>
+      <Card>
+        <CardHeader
+          subheader="Manage the frequent answer questions"
+          title="FAQ"
+        />
+        <Divider />
+        <CardContent>
+          <Typography color="textPrimary" gutterBottom variant="h6">
+            List
+          </Typography>
+        </CardContent>
+      </Card>
+      {props.faqs.map(faq => (
+        <Accordion key={faq.id} expanded={expanded === `panel${faq.id}`} onChange={handleChange(`panel${faq.id}`)}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>{faq.name}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{faq.status}</Typography>
+          </AccordionDetails>
+          <Divider />
+          <AccordionActions>
+            <Button size="small">Cancel</Button>
+            <Button size="small" color="primary">
+              Save
+            </Button>
+          </AccordionActions>
+        </Accordion>
+      ))}
+      <Fab
+        onClick={handleClickOpen}
+        className={classes.fab}
+        color="primary"
+        aria-label="add"
+      >
+        <AddIcon />
+      </Fab>
       <FaqDialog open={open} close={handleClose} />
-    </Card>
+    </div>
   );
 };
 
