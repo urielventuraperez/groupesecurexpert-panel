3a172cbc6d4ba770d43b35d59d296ed52cdd0d4a
Uriel <venturaperez.10101991@gmail.com>
2021-02-21 22:35:24 -0600

    Feature: add formik to faqs form

--- src/views/faq/FaqView/Form.js
+++ src/views/faq/FaqView/Form.js
@@ -6,38 +6,95 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import SaveIcon from '@material-ui/icons/Save';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+
+const FaqSchema = Yup.object().shape({
+  question: Yup.string()
+    .min(2, 'Too Short!')
+    .max(150, 'Too Long!')
+    .required('Required'),
+  answer: Yup.string()
+    .min(2, 'Too Short!')
+    .max(550, 'Too Long!')
+    .required('Required')
+});
 
 const FaqDialog = props => {
   return (
     <Dialog open={props.open} onClose={props.close}>
       <DialogTitle id="form-dialog-title">Add new FAQ</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="question"
-          label="Add the Question"
-          type="text"
-          fullWidth
-        />
-        <TextField
-          multiline
-          rows={10}
-          margin="dense"
-          id="answer"
-          label="Add the answer"
-          type="text"
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={props.close} color="secondary">
-          Cancel
-        </Button>
-        <Button variant="contained" onClick={props.close} color="primary" startIcon={<SaveIcon />}>
-          Save
-        </Button>
-      </DialogActions>
+      <Formik
+        initialValues={{
+          question: '',
+          answer: ''
+        }}
+        validationSchema={FaqSchema}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        {({
+              errors,
+              isValid,
+              dirty,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values
+            }) => (
+        <Form onSubmit={handleSubmit}>
+          <DialogContent>
+            <TextField
+              autoFocus
+              error={Boolean(touched.question && errors.question)}
+              helperText={touched.question && errors.question}
+              margin="dense"
+              id="question"
+              name="question"
+              label="Add the Question"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.question}
+              type="text"
+              fullWidth
+            />
+            <TextField
+              multiline
+              error={Boolean(touched.answer && errors.answer)}
+              helperText={touched.answer && errors.answer}
+              rows={10}
+              margin="dense"
+              id="answer"
+              name="answer"
+              label="Add the answer"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.answer}
+              type="text"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={props.close}>
+              Cancel
+            </Button>
+            <Button
+              disabled={!(isValid && dirty) || isSubmitting}
+              type="submit"
+              variant="contained"
+              onClick={props.close}
+              color="primary"
+              startIcon={<SaveIcon />}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Form>
+        )}
+      </Formik>
     </Dialog>
   );
 };
